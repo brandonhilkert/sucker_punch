33611c0f2e51dcb924f285de89131b5a49003fc9
Brandon Hilkert <brandonhilkert@gmail.com>
2013-06-30 16:00:27 -0400

    Add core extensions to support underscore

---
+++ lib/sucker_punch/core_ext.rb
@@ -0,0 +1,9 @@
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end if !defined?(::ActiveSupport) && !"".respond_to?(:underscore)
