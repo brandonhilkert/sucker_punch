d19d5add109b74ec5208349129762d9eb9363fb4
Brandon Hilkert <brandonhilkert@gmail.com>
2014-03-29 23:48:19 -0400

    Clear registry when Rails reloader is invoked

--- lib/sucker_punch/railtie.rb
+++ lib/sucker_punch/railtie.rb
@@ -2,6 +2,10 @@ module SuckerPunch
   class Railtie < ::Rails::Railtie
     initializer "sucker_punch.logger" do
       SuckerPunch.logger = Rails.logger
+
+      ActionDispatch::Reloader.to_prepare do
+        Celluloid::Actor.clear_registry
+      end
     end
   end
 end
