b4ea5c774357e252dc5b7b2d9427675dde276755
Brandon Hilkert <brandonhilkert@gmail.com>
2016-01-15 09:18:50 -0500

    Update doc for perform_in inline behavior

--- lib/sucker_punch/testing/inline.rb
+++ lib/sucker_punch/testing/inline.rb
@@ -1,26 +1,27 @@
 require 'sucker_punch'
 
-  # Include this in your tests to simulate
-  # immediate execution of your asynchronous jobs
-  #
-  # class LogJob
-  #   include SuckerPunch::Job
-  #
-  #   def perform(*args)
-  #     # log the things
-  #   end
-  # end
-  #
-  # To trigger asynchronous job:
-  #
-  #   LogJob.perform_async(1, 2, 3)
-  #
-  # Include inline testing lib:
-  #
-  # require 'sucker_punch/testing/inline"
-  #
-  # LogJob.perform_async(1, 2, 3) is now synchronous
-  #
+# Include this in your tests to simulate
+# immediate execution of your asynchronous jobs
+#
+# class LogJob
+#   include SuckerPunch::Job
+#
+#   def perform(*args)
+#     # log the things
+#   end
+# end
+#
+# To trigger asynchronous job:
+#
+#   LogJob.perform_async(1, 2, 3)
+#
+# Include inline testing lib:
+#
+# require 'sucker_punch/testing/inline"
+#
+# LogJob.perform_async(1, 2, 3) is now synchronous
+# LogJob.perform_in(1, 2, 3) is now synchronous
+#
 module SuckerPunch
   module Job
     module ClassMethods
